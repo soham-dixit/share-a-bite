4e913d3aa4daef0401a12bb92b77c58f60a5f80b
RAMYA A <125212940+ramyaa06@users.noreply.github.com>
2023-07-04 12:54:43 +0530

    Update and rename RuDeliverLogin.dart to RuDeliveryLogin.dart

---
+++ lib/login/RuDeliveryLogin.dart
@@ -0,0 +1,256 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+import 'package:get/get.dart';
+import 'package:share_a_bite/login/RuOpLogin.dart';
+import 'package:share_a_bite/widgets/CommonWidgets.dart';
+
+class RuDeliveryLogin extends StatefulWidget {
+  const RuDeliveryLogin({super.key});
+
+  @override
+  State<RuDeliveryLogin> createState() => _RuDeliveryLoginState();
+}
+
+class _RuDeliveryLoginState extends State<RuDeliveryLogin> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  final formKey = GlobalKey<FormState>();
+
+  final emailValidator = MultiValidator([
+    EmailValidator(errorText: 'Please enter a valid email ID'),
+    RequiredValidator(errorText: 'Please enter an email ID')
+  ]);
+
+  final passwordValidator = MultiValidator([
+    RequiredValidator(errorText: 'Password is required'),
+    MinLengthValidator(6, errorText: 'Password must be at least 6 digits long'),
+    PatternValidator(r'(?=.*?[#?!@$%^&*-])',
+        errorText: 'Password must have at least one special character')
+  ]);
+
+  RuOperatorLogin() {
+    Get.toNamed('/RuOpLogin');
+  }
+
+  RuDeliveryLogin(String email, String password) async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      Get.snackbar(
+        'Login Successful!',
+        'You have been logged in successfully',
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for this email.');
+        Get.snackbar(
+          'Error!',
+          'No user found for this email',
+        );
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for this user');
+        Get.snackbar(
+          'Error!',
+          'Wrong password provided for this user',
+        );
+      }
+    }
+  }
+
+  checkEmailExists(String email, String password) async {
+    final restaurantsRef = FirebaseFirestore.instance.collection('ru');
+    final querySnapshot =
+    await restaurantsRef.where('email', isEqualTo: email).get();
+    if (querySnapshot.docs.isEmpty) {
+      Get.snackbar("Error!", "Please register your account");
+    } else {
+      RuDeliveryLogin(email, password);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              vertical: MediaQuery.of(context).size.height / 15,
+              horizontal: MediaQuery.of(context).size.width / 15),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 18,
+              ),
+              const Text(
+                'Login as Recycling unit delivery partner',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              SvgPicture.asset(
+                'assets/users/ru.svg',
+                height: 120,
+                width: 120,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Enter your credentials",
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 14,
+                  // fontWeight: FontWeight.bold,
+                  color: Colors.grey.shade700,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 28,
+              ),
+              Container(
+                padding: EdgeInsets.all(28),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        controller: _emailController,
+                        textInputAction: TextInputAction.next,
+                        keyboardType: TextInputType.name,
+                        style: const TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 18,
+                          // fontWeight: FontWeight.bold,
+                        ),
+                        validator: emailValidator,
+                        cursorColor: Colors.black,
+                        decoration: InputDecoration(
+                          label: Text('Email ID'),
+                          labelStyle: TextStyle(
+                            color: Colors.grey.shade700,
+                          ),
+                          errorBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.red),
+                              borderRadius: BorderRadius.circular(10)),
+                          focusedErrorBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.red),
+                              borderRadius: BorderRadius.circular(10)),
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black12),
+                              borderRadius: BorderRadius.circular(10)),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black12),
+                              borderRadius: BorderRadius.circular(10)),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 22,
+                      ),
+                      TextFormField(
+                        controller: _passwordController,
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        textInputAction: TextInputAction.next,
+                        keyboardType: TextInputType.text,
+                        style: const TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 18,
+                          // fontWeight: FontWeight.bold,
+                        ),
+                        obscureText: true,
+                        validator: passwordValidator,
+                        cursorColor: Colors.black,
+                        decoration: InputDecoration(
+                          label: Text('Password'),
+                          labelStyle: TextStyle(
+                            color: Colors.grey.shade700,
+                          ),
+                          errorBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.red),
+                              borderRadius: BorderRadius.circular(10)),
+                          focusedErrorBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.red),
+                              borderRadius: BorderRadius.circular(10)),
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black12),
+                              borderRadius: BorderRadius.circular(10)),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black12),
+                              borderRadius: BorderRadius.circular(10)),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 22,
+                      ),
+                      MainButton(
+                        initialTitle: 'Login',
+                        onPressed: () {
+                          if (formKey.currentState!.validate()) {
+                            // inputs = [
+                            //   _emailController.text,
+                            //   _passwordController.text
+                            // ];
+                            checkEmailExists(_emailController.text,
+                                _passwordController.text);
+                          }
+                        },
+                      ),
+                      const SizedBox(
+                        height: 18,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Login as',
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 18,
+                              // fontWeight: FontWeight.bold,
+                              color: Color(0xFF000000),
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          GestureDetector(
+                            onTap: RuOperatorLogin,
+                            child: const Text(
+                              ' Operator?',
+                              style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 18,
+                                // fontWeight: FontWeight.bold,
+                                color: Color(0xFFF23F44),
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
