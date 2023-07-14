d98a70dd395e3068c981f54c98d9403b708ce83f
sohamdixit <sohamddixit@gmail.com>
2023-07-14 17:47:59 +0530

    done

--- lib/ngo_volunteer/AcceptedReq.dart
+++ lib/ngo_volunteer/AcceptedReq.dart
@@ -24,19 +24,24 @@ class _AcceptedReqVolState extends State<AcceptedReqVol> {
   List data = [];
   var result;
 
-  getData() async {
+getData() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     uid = prefs.getString('uid');
     final databaseReference = FirebaseDatabase.instance.ref();
     DatabaseEvent event = await databaseReference.once();
-    Map<dynamic, dynamic> databaseData = event.snapshot.value as Map;
-    keys_list = databaseData['volunteers'][uid]['distribution']['accepted']
-        .keys
-        .toList();
+    Map<dynamic, dynamic>? databaseData = event.snapshot.value as Map?;
+    keys_list = databaseData?['volunteers']?[uid]?['distribution']?['accepted']
+            ?.keys
+            ?.toList() ??
+        [];
     // print(keys_list);
     pendingListData.clear();
 
-    if (databaseData['volunteers'] != null) {
+    if (databaseData != null &&
+        databaseData['volunteers'] != null &&
+        databaseData['volunteers'][uid] != null &&
+        databaseData['volunteers'][uid]['distribution'] != null &&
+        databaseData['volunteers'][uid]['distribution']['accepted'] != null) {
       for (String key in keys_list) {
         dynamic pendingData =
             databaseData['volunteers'][uid]['distribution']['accepted'][key];
@@ -49,6 +54,7 @@ class _AcceptedReqVolState extends State<AcceptedReqVol> {
 
     return pendingListData;
   }
+
 
   @override
   Widget build(BuildContext context) {
